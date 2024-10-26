898fc4a3460e2bdde012b89980f5dac0fa56df81
NataliaJeszke <nataliajeszke8@gmail.com>
2024-10-26 13:46:52 +0200

    working search with filters

--- src/app/services/task-service.service.ts
+++ src/app/services/task-service.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Task } from '../models/task.model';
 import { formatDate } from '@angular/common';
 import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
+import { Task } from '../models/task.model';
+
 import { FiltersService } from './filters-service.service';
+import { SearchService } from './search-service.service';
 
 @Injectable({
   providedIn: 'root',
@@ -13,18 +16,20 @@ export class TaskService {
   tasks$ = this.tasksSubject.asObservable();
   filteredTasks$: Observable<Task[]>;
 
-  constructor(private filtersService: FiltersService) {
+  constructor(private filtersService: FiltersService, private searchService: SearchService) {
     this.filteredTasks$ = combineLatest([
       this.tasks$,
       this.filtersService.selectedDate$,
       this.filtersService.checkboxStateChange$,
       this.filtersService.statusChange$,
+      this.searchService.searchQuery$
     ]).pipe(
-      map(([tasks, selectedDate, checkboxState, status]) => {
+      map(([tasks, selectedDate, checkboxState, status, searchQuery]) => {
         return tasks.filter((task) => {
           let matchesDate = true;
           let matchesStatus = true;
           let matchesCompletion = true;
+          let matchesSearch = true;
     
           if (selectedDate) {
             const formattedDate = formatDate(selectedDate, 'dd-MM-yyyy', 'en-US');
@@ -39,8 +44,14 @@ export class TaskService {
           if (checkboxState) {
             matchesCompletion = task.completed === checkboxState;
           }
+
+          if (searchQuery) {
+            const lowerQuery = searchQuery.toLowerCase();
+            matchesSearch = task.title.toLowerCase().includes(lowerQuery) || 
+                            task.description.toLowerCase().includes(lowerQuery);
+          }
     
-          return matchesDate && matchesStatus && matchesCompletion;
+          return matchesDate && matchesStatus && matchesCompletion && matchesSearch;
         });
       })
     );
