d2166856d100524e0b09d3a746954958ad3a6371
NataliaJeszke <nataliajeszke8@gmail.com>
2024-10-28 15:30:40 +0100

    primeng with theme switcher

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component } from "@angular/core";
 import { SearchComponent } from "../search/search.component";
 import { UserComponent } from "../user/user.component";
+import { ToggleComponent } from "../ui/toggle/toggle.component";
 
 @Component({
   selector: "app-header",
   standalone: true,
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"],
-  imports: [SearchComponent, UserComponent]
+  imports: [SearchComponent, UserComponent, ToggleComponent]
 })
 
 export class HeaderComponent { }
